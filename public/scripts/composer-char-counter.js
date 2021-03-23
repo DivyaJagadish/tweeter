ebb4987be6452f8fef00bc2d8c4c723aceef1834
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-23 23:07:15 +0000

    Decreases the character count value and changes color on becoming negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,13 @@
 
 $(document).ready(function() {
   // --- our code goes here ---
-  console.log("document is ready");
+  $("#tweet-text").keyup(function(event) {
+    const length = $(this).val().length;
+    console.log(length);
+    const $formvariable =$(this).closest("form");// finds the closest form
+    $formvariable.find(".counter").html(140-length);//finds the variable in the form
+    if(length > 140) {
+      $formvariable.find(".counter").counter1;// Changes color on when value becomes -ve;
+    }
+});
 });
