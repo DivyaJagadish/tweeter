aa496d90c9371fa01accc4ff9435fc8f7d4c60ec
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-25 23:44:53 +0000

    clean up code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,13 +3,17 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
- 
-//escape from Xss
-const escape =  function(str) {
+
+////
+//FUNCTIONS
+////
+//escape  function to Escape from XSS
+const escape = function (str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
+
 const renderTweets = function (tweets) {
   $("#tweets-container").empty();
   // loops through tweets
@@ -20,11 +24,11 @@ const renderTweets = function (tweets) {
     $('#tweets-container').prepend($tweet);
   }
 };
-// create new tweets;
+// create new tweets
 const createTweetElement = function (tweetData) {
   // calculating no:of days before the tweet was created;
   let time = new Date().getTime()// today's time 
-  let daysago = Math.floor((time - tweetData.created_at)/(1000*60*60*24));//calculating difference in days to be displayed
+  let daysago = Math.floor((time - tweetData.created_at) / (1000 * 60 * 60 * 24));//calculating difference in days to be displayed
 
   const $tweet = $(`<article class="tweet">
    <header>
@@ -32,7 +36,7 @@ const createTweetElement = function (tweetData) {
         <img src=${escape(tweetData.user.avatars)}>
         <h3>${escape(tweetData.user.name)}</h3> 
     </div >
-      <h4>${escape(tweetData.user.handle)}</h4>
+    <h4>${escape(tweetData.user.handle)}</h4>
   </header>
   <div class="boxed"> ${escape(tweetData.content.text)}</div>
   <footer>
@@ -44,45 +48,50 @@ const createTweetElement = function (tweetData) {
     </p> 
   </footer>
 </article>`);
-return $tweet;
+  return $tweet;
 };
 
-// renderTweets(data);
-const loadtweets =function(){
+// load tweets into webpage
+const loadtweets = function () {
   $.ajax({
-    url : "/tweets",
-    method : "GET",
-  }).then(function(result){
+    url: "/tweets",
+    method: "GET",
+  }).then(function (result) {
     renderTweets(result);
   });
 };
+
 loadtweets();
+
 $(document).ready(function () {
 
-  $("#tweetform").on("submit",function(event){
-    event.preventDefault() ;
-    //  data from the form is serialised;
-    $("#error").hide();
+  //submit Event Handler
+  $("#tweetform").on("submit", function (event) {
+    event.preventDefault();
+    // data from the form is serialised;
+    $("#error").hide();//Hiding error msg 
     const $data = $(this).serialize();
-    const data =decodeURIComponent($data).substr(5);//converts to data with no URL component and substr as it contains text=
-    if(data.length !== 0 && $data.length <=140) {
+    const data = decodeURIComponent($data).substr(5);//converts to data with no URL component and substr as it contains text=
+    if (data.length !== 0 && $data.length <= 140) {
       $.ajax({
-        url : "/tweets",
-        method : "POST",
+        url: "/tweets",
+        method: "POST",
         data: $data
-      }).then(function(result){  
+      }).then(function (result) {
         loadtweets();
       });
-      $( '#tweetform' ).each(function(){
+      $('#tweetform').each(function () {
         this.reset();
         $(this).find(".counter").html(140)
-    });
-   } else if(data.length >140) {
-    $("#error").show();
-     $("#error").css({"opacity":"1"}).html(`<p id="errormsg">Error:Too long...Length exceeds 140 characters!!!!</p>`)
-   } else {
-    $("#error").show();
-    $("#error").css({"opacity":"1"}).html(`<p id="errormsg">No message.Type your tweet bedore submission!!!!</p>`)
-   }
+      });
+    } else if (data.length > 140) {
+      //displays error msg on length greater than 140
+      $("#error").show();
+      $("#error").css({ "opacity": "1" }).html(`<p id="errormsg">Error:Too long...Length exceeds 140 characters!!!!</p>`)
+    } else if(data.length === 0){
+       //displays error msg if no character is present.
+      $("#error").show();
+      $("#error").css({ "opacity": "1" }).html(`<p id="errormsg">No message.Type your tweet first!!!!</p>`)
+    }
   })
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,14 @@
-
-$(document).ready(function() {
+// char counter JS
+$(document).ready(function () {
   // --- our code goes here ---
-  $("#tweet-text").keyup(function(event) {
+  $("#tweet-text").keyup(function (event) {
     const length = $(this).val().length;
-    const $formvariable =$(this).closest("form");// finds the closest form
-    $formvariable.find(".counter").html(140-length);//finds the variable in the form
-    if(length > 140) {
-      $formvariable.find(".counter").css({"color":"red"});
-    }else {
-    $formvariable.find(".counter").css({"color":"black"});
+    const $formvariable = $(this).closest("form");// finds the closest form
+    $formvariable.find(".counter").html(140 - length);//finds the variable in the form
+    if (length > 140) {
+      $formvariable.find(".counter").css({ "color": "red" });
+    } else {
+      $formvariable.find(".counter").css({ "color": "black" });
     }
-});
+  });
 });
