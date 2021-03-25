ad8f9f7853abf3b90bc4b2f80d72af2f496ec1ec
Divya Jagadish <divyajagadish21@gmail.com>
2021-03-25 00:28:21 +0000

    AJAX GET

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,34 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
-// Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
 const renderTweets = function (tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
@@ -40,7 +12,7 @@ const renderTweets = function (tweets) {
     const $tweet = createTweetElement(tweet);
     $('#tweets-container').append($tweet);
   }
-}
+};
 // create new tweets;
 const createTweetElement = function (tweetData) {
   const $tweet = $(`<article class="tweet">
@@ -66,18 +38,16 @@ return $tweet;
 
 $(document).ready(function () {
 // renders old tweets
-const loadtweets =function(){
-  $.ajax({
-    url : "/tweets",
-    method : "GET",
-  }).then(function(result){
-    renderTweets(result);
-    console.log(result);
-  });
-};
-loadtweets();
+  const loadtweets =function(){
+    $.ajax({
+      url : "/tweets",
+      method : "GET",
+    }).then(function(result){
+      renderTweets(result);
+    });
+  };
+  loadtweets();
 // renderTweets(data);
-
   $("#tweetform").on("submit",function(event){
     event.preventDefault() ;
     //  data from the form is serialised;
@@ -86,12 +56,6 @@ loadtweets();
       url : "/tweets",
       method : "POST",
       data: $data
-    }).then(function(result){
-      console.log("Sucess",result);
     });
-
-
-
   })
-
 });
